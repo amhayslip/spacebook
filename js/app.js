107be9d32e71127b7764b475205218632b0ac404
amhayslip <amhayslip@gmail.com>
2015-12-13 11:47:25 +0200

    set up routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-angular.module('spacebook', []);
+angular.module('spacebook', ['ngRoute', 'ngResource'])
+  .config(function ($routeProvider) {
+
+    var routeConfig = {
+      controller: 'PostsCtrl',
+      templateUrl: 'spacebook-index.html',
+    };
+
+    $routeProvider
+      .when('/', routeConfig)
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
