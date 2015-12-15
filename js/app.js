c10d1a80210b7a03be9788d87912ca877d16f29c
amhayslip <amhayslip@gmail.com>
2015-12-15 10:01:30 +0200

    Remove other stuff too

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 angular.module('spacebook', ['ngRoute', 'ngResource'])
-  .config(function ($routeProvider) {
-    'use strict';
-
+  .config(function ($routeProvider) {g
     var routeConfig = {
       controller: 'PostsCtrl',
       templateUrl: 'spacebook-index.html'
